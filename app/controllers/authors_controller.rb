5e162999fd0e414deae4403e6ba9facfcca9743d
Joy Lin <vvlnote@hotmail.com>
2019-05-23 23:53:19 +0000

    Done.

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -7,9 +7,15 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    @author = Author.create!(author_params)
-
-    redirect_to author_path(@author)
+    #binding.pry
+    @author = Author.create(author_params)
+    #binding.pry
+    if @author.valid?
+      redirect_to author_path(@author)
+    else
+      render :new
+    end
+    
   end
 
   private
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,16 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
-
-    redirect_to post_path(@post)
+    #binding.pry
+    @post = set_post!
+    
+    if @post.update(post_params)
+      @post.save
+      redirect_to post_path(@post)
+    else
+      render :edit
+    end
+    
   end
 
   private
