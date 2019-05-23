759af8e355efaf27e3d66b8112a9c8ed4ca486ff
Joy Lin <vvlnote@hotmail.com>
2019-05-23 22:56:48 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
-  validates :content, length
+  validates :content, length: { minimum: 
 end
