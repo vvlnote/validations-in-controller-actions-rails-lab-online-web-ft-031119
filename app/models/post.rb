4a7dde25152977089e3dfb21a64ce593a92561fd
Joy Lin <vvlnote@hotmail.com>
2019-05-23 22:55:35 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ActiveRecord::Base
   validates :title, presence: true
-  validates :category, inclusion: {in: %w()
+  validates :category, inclusion: {in: %w(Fiction, Non-Fiction)}
 end
