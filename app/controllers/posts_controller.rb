5d8d270708bf48d4f4534a881d160dd4c46009a3
Joy Lin <vvlnote@hotmail.com>
2019-05-23 23:22:03 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,7 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post = Post.find(params[:id])
+    @post = set_post!
     @post.update(post_params)
     if @post.valid?
       @past.save
